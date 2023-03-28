079e9c8d72647935a5807686fab59e246a32c5eb
Wyatt Channings <wyattchannings@gmail.com>
2023-03-29 07:09:21 +0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package GradShow
+ */
+
+?>
+
+<footer id="colophon" class="site-footer">
+  <div class="site-info">
+    <p>Grad Show 2023 © All Rights Reserved</p>
+  </div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
